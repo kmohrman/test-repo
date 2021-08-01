90d077fb54159765e301ef4fbebed6d0924e01d8
kmohrman <36145025+kmohrman@users.noreply.github.com>
2021-08-01 14:50:56 -0400

    Revert "Modify print statement"

--- print_text.py
+++ print_text.py
@@ -1,6 +1,5 @@
 def print_some_text(s):
-    print("This is a function that prints some text here:",s)
-    print("This is the end of the function")
+    print("This is a function that prints some text:",s)
 
 def main():
     mystring = "Example text"
