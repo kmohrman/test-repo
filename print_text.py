f8f5f7eaa9aa69a5ebfe084eec306ee6d85a2674
Kelci Mohrman <kmohrman@nd.edu>
2021-08-01 14:47:07 -0400

    Use formatting in print statement

--- print_text.py
+++ print_text.py
@@ -1,8 +1,8 @@
 def print_some_text(s):
-    print("This is a function that prints some text:",s)
+    print(f"This is a function that prints some text: {s}")
 
 def main():
-    mystring = "Example text"
+    mystring = "Example text and some more text!"
     print_some_text(mystring)
 
 main()
